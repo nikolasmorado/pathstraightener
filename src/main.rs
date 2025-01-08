e997491bdc0509d46613d822a046bfb044df0bd5
Nikolas Morado <nik@mora.do>
2025-01-07 19:03:45 -0500

    feat: tokenizer done

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-// use std::collections::HashMap;
-
 #[derive(Debug)]
 enum Token {
     TagOpen(String),
@@ -13,53 +11,76 @@ enum Token {
 }
 
 fn main() {
-    // let a = String::from(r#"<a><p></p></a>"#);
-    let a = String::from(r#"<div class="tester"> "womp womp" the big cow said </div>"#);
+    let a = String::from(r#"<a><p></p></a>"#);
+    // let a = String::from(r#"<div class="tester"> "womp womp" the big cow said </div>"#);
     tokenize(a);
 }
 
 fn tokenize(s: String) {
-    // let mut char_map: HashMap<char, usize> = HashMap::new();
-    //
-    // for (i, c) in s.char_indices() {
-    //     if !char_map.contains_key(&c) {
-    //         char_map.insert(c, i);
-    //     }
-    // }
-
-    // for (i, c) in s.char_indices() {
-    //     if c == '"' {
-    //         if let Some(q_pos) = s[i+1..].find('"') {
-    //             for j in i..=i+1+q_pos {
-    //                 println!("{}", s.chars().nth(j).unwrap());
-    //             }
-    //             break;
-    //         }
-    //     }
-    // }
-
     let mut tokens: Vec<Token> = Vec::<Token>::new();
 
     let mut chars = s.chars().peekable();
 
+    let mut in_quote = false;
     while let Some(c) = chars.next() {
         match c {
             '<' => {
-                let mut tag = String::new(); 
-                while let Some(b) = chars.peek() {
-                    if b.is_whitespace() {
-                        break;
+                if chars.peek() == Some(&'/') {
+                    chars.next().unwrap();
+                    let mut tag = String::new();
+                    while let Some(b) = chars.peek() {
+                        if b == &'>' {
+                            break;
+                        }
+                        tag.push(chars.next().unwrap());
                     }
-                    tag.push(chars.next().unwrap());
+                    tokens.push(Token::TagEnd(tag));
+                } else {
+                    let mut tag = String::new();
+                    while let Some(b) = chars.peek() {
+                        if b.is_whitespace() || b == &'>' {
+                            break;
+                        }
+                        tag.push(chars.next().unwrap());
+                    }
+                    tokens.push(Token::TagOpen(tag));
                 }
-                tokens.push(Token::TagOpen(tag));
-
+            }
+            '=' => {
+                tokens.push(Token::AssignTo);
             }
             '>' => {
                 tokens.push(Token::TagClose);
             }
+            '"' => {
+                if in_quote {
+                    tokens.push(Token::QuoteClose);
+                    in_quote = false;
+                } else {
+                    tokens.push(Token::QuoteOpen);
+                    in_quote = true;
+                }
+            }
+            c if c.is_whitespace() && !in_quote => {}
+            c => {
+                let mut text = c.to_string();
 
-            _ => {}
+                let mut hit_equals = false;
+                while let Some(&b) = chars.peek() {
+                    if b == '"' || (!in_quote && (b == '<' || b == '>')) {
+                        break;
+                    } else if b == '=' {
+                        hit_equals = true;
+                        break;
+                    }
+                    text.push(chars.next().unwrap())
+                }
+                if hit_equals {
+                    tokens.push(Token::Attibute(text))
+                } else {
+                    tokens.push(Token::Text(text))
+                }
+            }
         }
     }
 
